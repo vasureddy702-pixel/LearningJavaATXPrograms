ada670b3b7e29d3a7b4aabb4d3078c179a2120c2
VasuReddy <vasureddy702@gmail.com>
2025-09-17 09:47:17 +0530

    Hello World

---
+++ src/JavaBasics/Print9Table.java
@@ -0,0 +1,26 @@
+package JavaBasics;
+
+public class Print9Table {
+
+    public static void main(String[] args) {
+
+
+        int table =9; int b=1;
+
+        System.out.printf("%dx1=%d\n", table, table);
+        System.out.printf("%dx2=%d\n", table, table*2);
+        System.out.printf("%dx3=%d\n", table, table*3);
+        System.out.printf("%dx4=%d\n", table, table*4);
+        System.out.printf("%dx5=%d\n", table, table*5);
+        System.out.printf("%dx6=%d\n", table, table*6);
+        System.out.printf("%dx7=%d\n", table, table*7);
+        System.out.printf("%dx8=%d\n", table, table*8);
+        System.out.printf("%dx9=%d\n", table, table*9);
+        System.out.printf("%dx10=%d\n", table, table*10);
+// %d -> int, byte, long, short, - data type
+// %s -> String
+// %f -> float, double,
+// %b ->boolean
+
+    }
+}
